2924caef79b0132461441d58ddcae3664e5b5687
mauromontecino <mauromontecino2@gmail.com>
2022-08-15 11:54:11 -0300

    components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <section>Main</section>
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className="NavBar">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Productos</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default NavBar;
