fb165749d1eac4f46ffc17d1c0426b120a28022d
mauromontecino <mauromontecino2@gmail.com>
2022-08-26 20:23:40 -0300

    .

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import estilos from "./Item.module.css";
+import ItemCount from "../ItemCount";
 
 const Item = ({ info }) => {
   return (
-    <span>
-      <a href="" className={estilos.products}>
-        <img src={info.image} alt="" />
-        <h2>{info.title}</h2>
-      </a>
-      <h3>{info.price}</h3>
-      <p>{info.description}</p>
-      <p>{info.stock}</p>
-    </span>
+    <>
+      <span>
+        <a href="" className={estilos.products}>
+          <img src={info.image} alt="" />
+          <h2>{info.title}</h2>
+        </a>
+        <h3>{info.price}</h3>
+        <p>{info.description}</p>
+        <p>Quedan: {info.stock} Unidades</p>
+      </span>
+      <ItemCount initial={1} stock={info.stock} />
+    </>
   );
 };
 
